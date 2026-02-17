bd255426585328c8135f2a818f7ba0cc012ee5a1
resident <165475039+resident72@users.noreply.github.com>
2026-02-18 00:17:48 +0100

    Add files via upload

---
+++ RPG.py
@@ -0,0 +1,7 @@
+
+def menu():
+    menu_options = ["Fight", "Quit"]
+    for x, name in enumerate(menu_options):
+        print([x], name)
+        
+menu()
---
+++ enemy.py
@@ -0,0 +1,3 @@
+class enemy:
+    enemy_health = 5
+    enemy_deamage = 2
