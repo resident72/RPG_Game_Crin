0d001f7489f42ca0380c899f7ef63c0a6ce811df
resident <165475039+resident72@users.noreply.github.com>
2026-02-18 11:52:44 +0100

    Update RPG.py

--- RPG.py
+++ RPG.py
@@ -1,7 +1,15 @@
 
+def main():
+    if 
+
+
 def menu():
-    menu_options = ["Fight", "Quit"]
+    menu_options = ["Fight", "Inventory", "Quit"]
     for x, name in enumerate(menu_options):
         print([x], name)
-        
-menu()
+
+def fight():
+    attack_options = ["Attack", "Defend", "Dodge"]
+    for x, name in enumerate(attack_options):
+        print([x], name)
+    
